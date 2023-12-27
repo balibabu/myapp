5ff49383b8bb25e323dd3388a58ecf556245e09d
balibabu <balibabuchauhan1@gmail.com>
2023-12-27 23:18:03 +0545

    added google verification

--- src/components/Apps/LinkShortner/LinkShortApp.jsx
+++ src/components/Apps/LinkShortner/LinkShortApp.jsx
@@ -24,11 +24,14 @@ export default function LinkShortApp() {
     const onCopy = () => {
         copyToClipboard(shortlink);
         setCopy('copied');
+        setTimeout(() => {
+            setCopy('copy');
+        }, 1000);
     }
     return (
         <div>
             <div className='m-0' style={{backgroundColor: "#403d39",height:"20vh"}}>
-                <h1 className='bg-primary m-0 p-0'>Shorten Your Link</h1>
+                <h1 className='bg-primary m-0 p-2'>Shorten Your Link</h1>
                 <p className='p-2 m-0' style={{color: "white"}}>Say goodbye to long, cumbersome URLs and hello to concise and shareable links! This Link Shortener empowers you to effortlessly shorten URLs, making them perfect for social media, messaging, and more.</p>
             </div>
             <div className='d-flex justify-content-center align-items-center' style={{ backgroundColor: "#403d39", height: "80vh", color: "wheat", paddingBottom: "60vh" }}>
