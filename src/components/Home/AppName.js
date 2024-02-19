80113ee42035f2b66a491c9a8ff8620d54fc7e32
balibabu <balibabuchauhan1@gmail.com>
2024-02-19 18:47:33 +0545

    nowrap to app names

--- src/components/Home/AppName.js
+++ src/components/Home/AppName.js
@@ -7,7 +7,7 @@ function AppName(props) {
                 <img src={props.image} style={{ height: "100px" }} className="card-img-top p-1" alt={props.appname} />
                 <div className="card-body p-0">
                     <hr className='m-0' />
-                    <b className="card-text">{props.appname}</b>
+                    <b className="card-text" style={{whiteSpace:'nowrap',overflow:'hidden'}}>{props.appname}</b>
                 </div>
             </div>
         </>
