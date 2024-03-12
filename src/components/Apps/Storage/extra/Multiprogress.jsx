0d6e73612d6ed1966253ac02ff2b2f22d15f455b
balibabu <balibabuchauhan1@gmail.com>
2024-03-12 16:22:38 +0545

    multiprogress ui update

--- src/components/Apps/Storage/extra/Multiprogress.jsx
+++ src/components/Apps/Storage/extra/Multiprogress.jsx
@@ -7,13 +7,14 @@ export default function Multiprogress({ progressList }) {
     return (
         <>
             {av > 0 &&
-                <div className='my-2 mx-3'>
-                    <div className='mb-1'><Progress {...{ title: 'Uploading File', progress: av, height: '1.5rem' }} /></div>
-                    <div className='row m-0 '>
+                <div className='my-2 mx-3 position-relative rounded-3 overflow-hidden'>
+                    <div className='mb-1'><Progress {...{ title: 'Uploading File', progress: av, height: '1.5rem', css: 'rounded-0' }} /></div>
+                    <div className='row m-0' style={{ position: 'absolute', bottom: '0', width: '100%' }}>
                         {progressList.map((progress, index) => {
-                            return <div className='col ps-0 pe-0' key={index}><Progress {...{ progress, css: 'rounded-0 text-primary', height: '.3rem' }} /></div>
+                            return <div className='col ps-0 pe-0' key={index}><Progress {...{ progress, css: 'rounded-0 text-info', height: '0.3rem',bg:'info' }} /></div>
                         })}
                     </div>
+                    {/* <div className='row m-0' style={{ position: 'absolute', top: '1rem' }}>bali</div> */}
                 </div>
             }
         </>
--- src/http/_baseURL.js
+++ src/http/_baseURL.js
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = 'http://127.0.0.1:8000';
+// export const MEDIA_BASE_URL = 'http://127.0.0.1:8000';
 export const API_BASE_URL = 'https://balib.pythonanywhere.com';
 export const MEDIA_BASE_URL = 'https://balibabu.pythonanywhere.com';
