ee53195b1b1a780e04bc7af0bd8226456c892b7c
balibabu <balibabuchauhan1@gmail.com>
2024-04-28 23:23:53 +0545

    toast progressbar added

--- src/components/Shared/Toast.jsx
+++ src/components/Shared/Toast.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import VariableContext from '../Contexts/VariableContext';
 
 export default function Toast() {
@@ -16,24 +16,36 @@ export default function Toast() {
 }
 
 function ToasMessageCard({ msg, closeHandler, toastColor }) {
+    const [width, setWidth] = useState(100);
+    const timeoutTime = 5000;
+    const intervalTime = 50;
+
     useEffect(() => {
         const id = setTimeout(() => {
             closeHandler(msg.id);
-        }, 5000);
+        }, timeoutTime);
+        const interval = setInterval(() => {
+            setWidth((prev) => prev - 100 / (timeoutTime / intervalTime));
+        }, intervalTime);
+
         return () => {
+            clearInterval(interval);
             clearTimeout(id);
         }
     }, [])
 
     return (
-        <div className='my-1 px-2 py-1 rounded-3 d-flex justify-content-between' style={{ width: '250px', backgroundColor: toastColor[msg.bg] || 'rgb(200,255,200)' }}>
-            <div>
-                <div className="me-auto">{msg.title}</div>
-                <div className='text-end' style={{fontSize:'14px',lineHeight:'1.2'}}>{msg.content}</div>
+        <div className='mt-1 rounded-3' style={{ overflow: 'hidden' }}>
+            <div className='px-1 d-flex justify-content-between' style={{ width: '250px', backgroundColor: toastColor[msg.bg] || 'rgb(200,255,200)' }}>
+                <div>
+                    <div className="me-auto">{msg.title}</div>
+                    <div className='text-end' style={{ fontSize: '14px', lineHeight: '1.2' }}>{msg.content}</div>
+                </div>
+                <div className='d-flex align-items-center'>
+                    <button className='btn-close' onClick={() => closeHandler(msg.id)}></button>
+                </div>
             </div>
-            <div className='d-flex align-items-center'>
-                <button className='btn-close' onClick={() => closeHandler(msg.id)}></button>
-            </div>
+            <div style={{ width: `${width}%`, height: '3px', backgroundColor: `rgb(${255 - width * 2},0,${width * 2 + 55})` }}></div>
         </div>
     );
 }
